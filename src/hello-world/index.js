df3e921d90dd0519bdad3130a065427a2c80b846
Fränk Klein <frank@kleinconsulting.lu>
2024-05-16 10:20:16 +0200

    Blocks setup: Add block registration in JavaScript.

--- src/hello-world/index.js
+++ src/hello-world/index.js
@@ -1,1 +1,13 @@
-console.log( 'Hello World' );
+import { registerBlockType } from '@wordpress/blocks';
+
+registerBlockType(
+	'bdm/hello-world',
+	{
+		edit: function() {
+			return <p>Hello World</p>
+		},
+		save: function() {
+			return <p>Hello World</p>
+		}
+	}
+);
